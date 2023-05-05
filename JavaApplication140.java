ff596b91caee173d5afd3f63750a8cf030b3eb61
Abutalib1 <117555993+Abutalib1@users.noreply.github.com>
2023-05-05 17:20:13 +0300

    Add files via upload

---
+++ JavaApplication140.java
@@ -0,0 +1,30 @@
+package javaapplication140;
+public class JavaApplication140 {
+    public static void main(String[] args) {
+        int n = 9;
+        PrintPyramid(9,1);       
+    }
+    public static void PrintPyramid(int n,int i){
+        if ( n != 0 ){
+            spaces(n-1); // (n-1) because the left spaces will be for the stars
+            stars(i);
+            System.out.println(""); // to jump to the next row
+            PrintPyramid(n-1,i+1); // recursive calling
+        }
+    }
+    public static void spaces(int n){
+        // base case is when n = 0
+        if (n != 0){
+            System.out.print(" ");
+            spaces(n-1); // recursive calling
+        }
+    }
+    public static void stars(int n){
+        // base case is when n = 0
+        if (n != 0){
+            System.out.print("* ");
+            stars(n-1); // recursive calling
+        }
+    }
+    
+}
